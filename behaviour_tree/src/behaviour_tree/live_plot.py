ae5093593a6e225ac465bd731c2ee92b2b11bb4a
satria <satriapratama100@gmail.com>
2022-05-10 14:35:51 +0700

    adding data writing into txt file

--- behaviour_tree/src/behaviour_tree/live_plot.py
+++ behaviour_tree/src/behaviour_tree/live_plot.py
@@ -10,8 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import rospy
+import os
 
-import behaviour_tree.condition as cond 
+import behaviour_tree.condition as cond
 
 rospy.init_node("local_planner", anonymous=True)
 mission_waypoint = cond.mission_waypoint()
@@ -25,30 +26,33 @@ ax2 = plt.subplot2grid(gridsize, (0,6),colspan=3,rowspan=3)
 
 x = np.linspace(0,10,100)
 y = np.linspace(5,5,100)
-
+txt_path = os.path.abspath(__file__+"/../output/plot.txt")
 
 def animate(i):
     try:
         waypoint = cond.waypoint()
         curr_state = cond.pose()
         obstacles = cond.obstacles_classifier()
+        manuver = cond.manuver_type()
         x_ = []
         y_ = []
-        idx = cond.get_start_and_lookahead_index(waypoint,curr_state[0],curr_state[1],0)
-        yaw = curr_state[2]-waypoint[idx[0]][2]
+        # idx = cond.get_start_and_lookahead_index(waypoint,curr_state[0],curr_state[1],0)
+        # yaw = curr_state[2]-waypoint[idx[0]][2]
         for obstacle in obstacles:
             x, y = cond.occupancy_grid(obstacle,1)
-            x_ = x_+x
-            y_ = y_+y
+            x_ +=x
+            y_ +=y
         x_ = np.array(x_)
         y_ = np.array(y_)
-        x_ = x_ + curr_state[0]
-        y_ = y_ + curr_state[1]
+        x_ += curr_state[0]
+        y_ += curr_state[1]
+        
+        #waypoints
         x = []
         y = []
-        for i in range (len(waypoint)):
-            x.append(waypoint[i][0])
-            y.append(waypoint[i][1])
+        for j in range (len(waypoint)):
+            x.append(waypoint[j][0])
+            y.append(waypoint[j][1])
         ax1.cla()
         
         ax1.plot(mission_waypoint[:,0],mission_waypoint[:,1], color='0.8', linewidth = 2.5, ls="--")
@@ -57,15 +61,26 @@ def animate(i):
         ax1.scatter(x_, y_, label="Object",color='black')
         ax1.set_xlabel('x (m)')
         ax1.set_ylabel('y (m)')
-        ax1.set_title('Trajectory')
+        ax1.set_title('Waypoints')
         ax1.set_ylim([a,b])
         ax1.grid()
         
-        ax2.plot(i,curr_state[3])
-        ax2.set_xlabel('t (s)')
+        ax2.scatter(i,curr_state[3],color='black')
+        print(manuver)
+        ax2.set_xlabel('i')
         ax2.set_ylabel('v (m/s)')
         ax2.set_title('Velocity')
         ax2.set_xlim([i-10,i+10])
+        # SaveTxt
+        
+        with open(txt_path, 'a') as f:
+            f.write(('%g;' * 4) % (curr_state[0],curr_state[1],curr_state[2],curr_state[3]))
+            f.writelines(manuver+';')
+            f.writelines(str(x)+';')
+            f.writelines(str(y)+';')
+            f.writelines(str(x_)+';')
+            f.writelines(str(y_)+';')
+            f.writelines('\n')
     except:
         pass
 
