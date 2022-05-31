yaw fixing, velocity calculation fixing

behaviour_tree/src/behaviour_tree/actions.py
@@ -52,7 +52,7 @@ def track_speed(curr_state, mission_waypoint, v_ts, a_max):
     # print("curr state action")
     # print(curr_state)
     freq = rospy.get_param('~freq', 10) # Hz
-    ld_dist = rospy.get_param('~ld_dist', 10.0) # m
+    ld_dist = rospy.get_param('~ld_dist', 15.0) # m
     
     pub = rospy.Publisher('/wp_planner', Planner, queue_size=1)
     rate = rospy.Rate(freq) # Hz
@@ -141,7 +141,7 @@ def track_speed(curr_state, mission_waypoint, v_ts, a_max):
     msg.v = v
     msg.curv = curv
     
-    axx = cond.waypoint()
+    # axx = cond.waypoint()
     # # Plot for debuging
     # idx = [i for i in range(len(v))]
     
@@ -178,7 +178,7 @@ def track_speed(curr_state, mission_waypoint, v_ts, a_max):
 
 def follow_leader(curr_state, mission_waypoint, waypoint, a_max):
     freq = rospy.get_param('~freq', 10) # Hz
-    ld_dist = rospy.get_param('~ld_dist', 10.0) # m
+    ld_dist = rospy.get_param('~ld_dist', 15.0) # m
     
     pub = rospy.Publisher('/wp_planner', Planner, queue_size=1)
     rate = rospy.Rate(freq) # Hz
@@ -198,7 +198,7 @@ def follow_leader(curr_state, mission_waypoint, waypoint, a_max):
     #Asumsi ketika selisih jarak 5m, kecepatan kendaraan biar 1m/s
     Kgap = 0.2
     
-    d_min = 2.4
+    d_min = 3.4
     l_veh = 2.4
     d_des = max(l_veh*curr_state[3]/5, d_min)
     d_act = cond.leader_distance(curr_state,waypoint)
@@ -303,10 +303,10 @@ def switch_lane(curr_state,mission_waypoints,pred_time,a_max):
     # print(curr_state)
     freq = rospy.get_param('~freq', 10) # Hz
     ld_dist = rospy.get_param('~ld_dist', 10.0) # m
-    n_offset = rospy.get_param('~n_offset', 9) # m
-    offset = rospy.get_param('~offset', 1.5) # m
-    c_location = rospy.get_param('~c_location', [-0.2, 1.2, 2.2]) # m
-    c_rad = rospy.get_param('~c_rad', [0.9, 0.9, 0.9]) # m
+    n_offset = rospy.get_param('~n_offset', 3) # m
+    offset = rospy.get_param('~offset', 3.) # m
+    c_location = rospy.get_param('~c_location', [-0.2, 1.2, 2.7]) # m
+    c_rad = rospy.get_param('~c_rad', [1.2, 1.2, 1.2]) # m
     d_weight = rospy.get_param('~d_weight', 0.5)
     
     # Create local planner classes
@@ -347,6 +347,7 @@ def switch_lane(curr_state,mission_waypoints,pred_time,a_max):
     
     # Get lookahead index
     ld_idx = lp.get_lookahead_index(curr_state[0], curr_state[1])
+    print('curr_state yaw:', curr_state[2])
     print('Lookahead yaw: ', mission_waypoints[ld_idx][2])
     
     # Get offset goal states, pada acuan lokal
@@ -464,44 +465,47 @@ def switch_lane(curr_state,mission_waypoints,pred_time,a_max):
     # plt.ylabel('v')
     # plt.show()
     # Publish the message
-    print('currstate vs yaw pertama waypoint yang digenerate')
-    print(curr_state)
-    print(yaw[0])
-    pub = rospy.Publisher('/wp_planner', Planner, queue_size=1)
-    pub.publish(msg)
-    # while(True):
-    #     # print(msg)
-    #     pub = rospy.Publisher('/wp_planner', Planner, queue_size=1)
-    #     rate = rospy.Rate(freq) 
-    #     # print("wp trackspeed")
-    #     # print(msg) 
-    #     pub.publish(msg)
-    #     obstacles = cond.obstacles_classifier()
-    #     obj_ = []
-    #     x_ = []
-    #     z_ = []
-    #     for obstacle in obstacles:
-    #         x, z = cond.occupancy_grid(obstacle,pred_time)
-    #         x_+=x
-    #         z_+=z
-    #     for i in range (len(x_)):
-    #         obj_.append([x_[i],z_[i]])
-    #     obj_ = np.array(obj_)  
-    #     # Collision Check
-    #     # return true if free collision
-    #     # return false if collision
-    #     coll = cc.collision_check([path_generated[0][bp]], obj_)
-    #     distance = cond.d_rem(cond.pose(), cond.waypoint())
-    #     print("distance")
-    #     print(distance)
-    #     # print('panjang waypointtt::  ',len(cond.waypoint()))
-    #     if not coll[0]:
-    #         print("STATUS: Collision detected, switching lane end")
-    #         break
-    #     elif (distance<=0.25):
-    #         print("STATUS: Vehicle has successfully switched lane")
-    #         break
-    #     print("Vehicle is switching lane, no collision detected")
+    # print('currstate')
+    # print(curr_state)
+    # print('vs waypoint yang digenerate')
+    # print('awal   : ',x[0],y[0],yaw[0],v[0])
+    # print('akhir  : ',x[-1],y[-1],yaw[-1],v[-1])
+    # pub = rospy.Publisher('/wp_planner', Planner, queue_size=1)
+    # pub.publish(msg)
+    while(True):
+        # print(msg)
+        pub = rospy.Publisher('/wp_planner', Planner, queue_size=1)
+        rate = rospy.Rate(freq) 
+        # print("wp trackspeed")
+        # print(msg) 
+        pub.publish(msg)
+        obstacles = cond.obstacles_classifier()
+        obj_ = []
+        x_ = []
+        z_ = []
+        for obstacle in obstacles:
+            x, z = cond.occupancy_grid(obstacle,pred_time)
+            x_+=x
+            z_+=z
+        for i in range (len(x_)):
+            obj_.append([x_[i],z_[i]])
+        obj_ = np.array(obj_)  
+        # Collision Check
+        # return true if free collision
+        coll = cc.collision_check([path_generated[0][bp]], obj_)
+        print('',coll)
+        # return false if collisionrated[0][bp]], obj_)
+        distance = cond.d_rem(cond.pose(), cond.waypoint())
+        print("distance")
+        print(distance)
+        # print('panjang waypointtt::  ',len(cond.waypoint()))
+        if not coll[0]:
+            print("STATUS: Collision detected, switching lane end")
+            break
+        elif (distance<=0.25):
+            print("STATUS: Vehicle has successfully SWITCH LANE")
+            break
+        print("Vehicle is switching lane, no collision detected")
 
    
 def decelerate_to_stop(curr_state, xf,yf,yawf, a_max):

behaviour_tree/src/behaviour_tree/condition.py
@@ -72,7 +72,7 @@ def state_callback(msg_nav):
     
     local_state['x'] = msg_nav.x
     local_state['y'] = msg_nav.y
-    local_state['v'] = np.sqrt(msg_nav.vx**2 + msg_nav.vy**2)
+    local_state['v'] = np.sqrt(msg_nav.vx**2 + msg_nav.vy**2) if (msg_nav.vx > 0.05 ) else 0.
     local_state['yaw'] = msg_nav.yaw_est
      
 def perception_callback(data):
@@ -97,11 +97,11 @@ def planner_callback(planner_msg):
     wp_planner['v'] = planner_msg.v
     wp_planner['curv'] = planner_msg.curv
 
-def mission_waypoint(mtype='real',file='wp_22Mei1031.npy'):
+def mission_waypoint(mtype='simulation',file='wp_22Mei1031.npy'):
     if (mtype=='real'):
         mission_waypoint = np.load(os.path.abspath(__file__+'/../../../../waypoints/waypoints/'+file))
     elif (mtype=='simulation'):
-        mission_waypoint = np.array(waypoints_dummies().diagonal())
+        mission_waypoint = np.array(waypoints_dummies().straight())
     # print('waypoint misi: ',len(mission_waypoint))
     return mission_waypoint
     
@@ -186,7 +186,7 @@ def obstacles_classifier():
     global obj
     
     # Uncoment kalau dummies
-    obj = obstacle().slow()
+    # obj = obstacle().slow()
     
     # print('x,z = (',obj['xc'],obj['zc'],')')
     # print('vx,vz = (',obj['vxc'],obj['vzc'],')')
@@ -207,6 +207,7 @@ def obstacles_classifier():
         }
         obs.append(obj_row)
         a = b
+    # print('ada',len(obs[0]['obj_x']))
     
     return obs
 
@@ -230,8 +231,8 @@ def d_rem(curr_state,mission_waypoint):
 # - waypoint = [x,y,yaw,curve,v]
 # - obj_ adalah matriks objek dalam occupancy grid
 def leader_selection(curr_state,waypoint):
-    c_location = rospy.get_param('~c_location', [-0.2, 1.2, 2.2]) # m
-    c_rad = rospy.get_param('~c_rad', [0.9, 0.9, 0.9]) # m
+    c_location = rospy.get_param('~c_location', [-0.2, 1.2, 2.7]) # m
+    c_rad = rospy.get_param('~c_rad', [1., 1., 1.]) # m
     d_weight = rospy.get_param('~d_weight', 0.5)
     #Colllision Check Class
     cc = CollisionChecker(c_location, c_rad, d_weight)
@@ -244,14 +245,21 @@ def leader_selection(curr_state,waypoint):
 
     idx = get_start_and_lookahead_index(waypoint,curr_state[0],curr_state[1],0)
     yaw = curr_state[2]-waypoint[idx[0]][2]
+    print('yawiii: ', yaw)
     
     x_p = []
     y_p = []
     t_p = []
     for i in range (idx[0],len(waypoint)):
-        x_p.append(waypoint[i][0]-waypoint[idx[0]][0])
-        y_p.append(waypoint[i][1]-waypoint[idx[0]][1])
-        t_p.append(waypoint[i][2]-waypoint[idx[0]][2])
+        yawt = waypoint[idx[0]][2]
+        x = waypoint[i][0]-waypoint[idx[0]][0]
+        y = waypoint[i][1]-waypoint[idx[0]][1]
+        z = waypoint[i][2]-yawt
+        # x_p.append(x)
+        # y_p.append(y)
+        x_p.append(x*np.cos(-yawt)-y*np.sin(-yawt))
+        y_p.append(x*np.sin(-yawt)+y*np.cos(-yawt))
+        t_p.append(z)
     path = [x_p, y_p, t_p]
     paths = [path]
 
@@ -268,6 +276,8 @@ def leader_selection(curr_state,waypoint):
         # return True if the path is free collision
         # return False if the path is collided
         coll = cc.collision_check(paths, obj_)
+        print('tidak ada leader: ')
+        print(coll)
 
         # Jika dicek terjadi collision
         # yang artinya ada leader, karena ada obstacle pada path
@@ -283,18 +293,21 @@ def is_leader_ex(curr_state,waypoint):
     if (len(id)):
         return True
     else:
+        print('Kosong')
         return False
     
 def leader_velocity(curr_state,waypoint):
     vzc = leader_selection(curr_state,waypoint)[1]
     if vzc==[]:
         return None
+    print('Kecepatan leader: ',vzc[0])
     return vzc[0]
 
 def leader_distance(curr_state,waypoint):
     zc = leader_selection(curr_state,waypoint)[2]
     if zc==[]:
         return None
+    print('Jarak leader :',zc[0])
     return zc[0]
 
 def get_start_and_lookahead_index(waypoint, x, y, ld_dist):
@@ -336,18 +349,18 @@ def occupancy_grid(obstacle, pred_time):
     vy_ = obstacle['vxc']
 
     # # Real
-    for i in range (len(obstacle['obj_x'])):
-        x = obstacle['obj_z'][i]
-        y = obstacle['obj_x'][i]
-        x_.append(x*np.cos(yaw)-y*np.sin(yaw))
-        y_.append(x*np.sin(yaw)+y*np.cos(yaw))
-    
-    # Simulasi
     # for i in range (len(obstacle['obj_x'])):
     #     x = obstacle['obj_z'][i]
-    #     y = obstacle['obj_x'][i]
-    #     x_.append(x)
-    #     y_.append(y)
+    #     y = -obstacle['obj_x'][i]
+    #     x_.append(x*np.cos(yaw)-y*np.sin(yaw))
+    #     y_.append(x*np.sin(yaw)+y*np.cos(yaw))
+    
+    # Simulasi
+    for i in range (len(obstacle['obj_x'])):
+        x = obstacle['obj_z'][i]
+        y = -obstacle['obj_x'][i]
+        x_.append(x)
+        y_.append(y)
     
     for i in range(pred_time-1):
         for j in range(len(x_)):
@@ -365,10 +378,10 @@ def possible_path(curr_state,mission_waypoints, pred_time):
     # curr_state = curr_state + [0,0,np.pi,0]
     # print(curr_state)
     ld_dist = rospy.get_param('~ld_dist', 10.0) # m
-    n_offset = rospy.get_param('~n_offset', 9) # m
-    offset = rospy.get_param('~offset', 1.5) # m
-    c_location = rospy.get_param('~c_location', [-0.2, 1.2, 2.2]) # m
-    c_rad = rospy.get_param('~c_rad', [0.9, 0.9, 0.9]) # m
+    n_offset = rospy.get_param('~n_offset', 3) # m
+    offset = rospy.get_param('~offset', 3.) # m
+    c_location = rospy.get_param('~c_location', [-0.2, 1.2, 2.7]) # m
+    c_rad = rospy.get_param('~c_rad', [1.2, 1.2, 1.2]) # m
     d_weight = rospy.get_param('~d_weight', 0.5)
     print("a")
     # waypoints = waypoints-[waypoints[0][0],waypoints[0][1],0,0,0]
@@ -376,15 +389,19 @@ def possible_path(curr_state,mission_waypoints, pred_time):
     cc = CollisionChecker(c_location, c_rad, d_weight)
     print("b")
     
-    
     ### Generate feasible paths for collision checker
     # Get lookahead index
     ld_idx = lp.get_lookahead_index(curr_state[0], curr_state[1])
     # print('Lookahead: ', mission_waypoints[ld_idx])
-    print("c")
+    print("c: indeks lookahead")
+    print(ld_idx)
     # Get offset goal states
     g_set = lp.get_goal_state_set(mission_waypoints[ld_idx], curr_state)
     print("d")
+    print('curr_state yaw:', curr_state[2])
+    print('Lookahead yaw: ', mission_waypoints[ld_idx][2])
+    print('lookahead x,y: ',mission_waypoints[ld_idx][0],mission_waypoints[ld_idx][1])
+    
     # Plan paths
     path_generated = lp.plan_paths(g_set)
     
@@ -413,7 +430,6 @@ def possible_path(curr_state,mission_waypoints, pred_time):
             return True
         else:
             False
-
 
 # rospy.init_node('condition', anonymous=True)
 # Set callback data and variables

behaviour_tree/src/behaviour_tree/local_planner/local_planner.py
@@ -12,6 +12,7 @@ import numpy as np
 import copy
 from behaviour_tree.local_planner.spiral_generator import SpiralGenerator
 from math import sin, cos, pi, sqrt
+import matplotlib.pyplot as plt
 
 class LocalPlanner(object):
     def __init__(self, waypoints, ld_dist, num_paths, path_offset):
@@ -96,6 +97,7 @@ class LocalPlanner(object):
         elif goal_t < -np.pi:
             goal_t += 2*np.pi
             
+        print(goal_t)
         goal_state_set = []
         
         for i in range(self._num_paths):
@@ -108,7 +110,7 @@ class LocalPlanner(object):
                                    goal_y + y_offset,
                                    goal_t,
                                    goal_v])
-        
+            print(goal_state_set[i][0],goal_state_set[i][1],goal_state_set[i][2])
         return goal_state_set
     
     def plan_paths(self, goal_state_set):
@@ -130,11 +132,14 @@ class LocalPlanner(object):
             if np.linalg.norm([path[0][-1] - goal_state[0], 
                                path[1][-1] - goal_state[1], 
                                path[2][-1] - goal_state[2]]) > 0.1:
+                # paths.append(path)
                 path_validity.append(False)
             else:
                 paths.append(path)
                 path_validity.append(True)
-        
+            # plt.plot(path[0],path[1])
+            # plt.show()
+            
         return paths, path_validity
     
     def transform_paths(self, paths, ego_state):

persepsi/src/persepsi/track.py
@@ -149,6 +149,7 @@ def detect(opt):
     for frame_idx, (path, depth, distance, depth_scale, img, im0s, color_intrin, aligned_df, verts, vid_cap, s) in enumerate(dataset):
         if rospy.is_shutdown():
             break
+        t1b = t1
         t1 = time_sync()
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -229,8 +230,8 @@ def detect(opt):
                             xy = rs.rs2_project_point_to_pixel(color_intrin, [x_m, y_m, z_m])
                             width = 848
                             height = 480
-                            vx = output[5] / (t5-t1)
-                            vz = output[9] / (t5-t1)
+                            vx = output[5] / (t1-t1b)
+                            vz = output[9] / (t1-t1b)
                             w = output[2]
                             h = output[3]
                             x1 = max(int(xy[0] - w / 2), 0) + 1
@@ -247,7 +248,7 @@ def detect(opt):
                                 break
                             zs = object_points[:, 2]
                             
-                            # Get z percentile 40 (where most likely the object blob is, 
+                            # Get first quartile z (where most likely the object blob is, 
                             # think of it like medians but more likely to pick the foreground object instead of the background)
                             z_ = np.percentile(zs, 25)
                             print("z_")
@@ -367,7 +368,7 @@ if __name__ == '__main__':
     parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
     parser.add_argument('--save-txt', default='true', help='save MOT compliant results to *.txt')
     # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int, default='0', help='filter by class: --class 0, or --class 16 17')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 16 17')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--evaluate', action='store_true', help='augmented inference')

pkg_ta/nodes/vD_control_2d_ukf_localization.py
@@ -15,7 +15,7 @@ from behaviour_tree.msg import Planner
 import behaviour_tree.condition as cond
 
 min_throttle = 0.0
-max_throttle = 0.25
+max_throttle = 0.5
 max_steer_arduino = 28.
 min_steer_arduino = -28.#-35.
 
@@ -44,11 +44,11 @@ def callback(msg_nav):
 
     state['x'] = msg_nav.x
     state['y'] = msg_nav.y
-    state['v'] = np.sqrt(msg_nav.vx**2 + msg_nav.vy**2)
-    # state['yaw'] = msg_nav.yaw_est    
+    state['v'] = np.sqrt(msg_nav.vx**2 + msg_nav.vy**2) if (msg_nav.vx > 0.05 ) else 0.
+    state['yaw'] = msg_nav.yaw_est
     # state['yaw'] = atan2(2 * (nav_msg.pose.pose.orientation.w * nav_msg.pose.pose.orientation.z + nav_msg.pose.pose.orientation.x * nav_msg.pose.pose.orientation.y), 1 - 2 * (nav_msg.pose.pose.orientation.y**2 + nav_msg.pose.pose.orientation.z**2))
     # state['yaw'] = msg_nav.yaw_imu
-    state['yaw'] = msg_nav.yaw_est if (state['v'] < 0.4) else wrap_angle(msg_nav.yaw_dydx - np.pi/2)
+    # state['yaw'] = msg_nav.yaw_est if (state['v'] < 0.4) else wrap_angle(msg_nav.yaw_dydx - np.pi/2)
 
     RUN = True
 
@@ -81,15 +81,16 @@ kv = rospy.get_param('~kv', 1.00)
 lateral_dead_band = rospy.get_param('~lateral_dead_band', 0.025)
 sat_lat_max = rospy.get_param('~sat_lat_max', 0.6109)
 sat_lat_min = rospy.get_param('~sat_lat_min', -0.4887)
-# waypoints_path = rospy.get_param('~waypoints_path', 'wp_monev_baru.npy')
-# waypoints_path = os.path.abspath(__file__+'/../../src/waypoints/waypoints/' + waypoints_path)
+# waypoints_path = rospy.get_param('~waypoints_path', 'wp_21mei_1435.npy')
+# waypoints_path = 'wp_22Mei1031.npy'
+# waypoints_path = os.path.abspath(__file__+'/../../../waypoints/waypoints/' + waypoints_path)
 
 feed_forward_params = np.array([ff_1, ff_2])
 sat_long = np.array([sat_long_min, sat_long_max])
 sat_lat = np.array([sat_lat_min, sat_lat_max])
 
 # waypoints = np.array(np.load(waypoints_path))
-waypoints = cond.mission_waypoint(mtype='simulation')
+waypoints = cond.mission_waypoint()
 
 # Create the controller object
 controller = Controller(kp, ki, kd, feed_forward_params, sat_long,\
@@ -140,22 +141,24 @@ while not rospy.is_shutdown():
         
         # Change message to waypoints format
         ## [x, y, yaw, v, curv]
-        for i in range(len(planner['x'])):
-            best_wp.append([planner['x'][i], planner['y'][i], planner['yaw'][i],
-                           planner['v'][i], planner['curv'][i]])
+        planner_copy = planner.copy()
+        for i in range(len(planner_copy['x'])):
+            best_wp.append([planner_copy['x'][i], planner_copy['y'][i], planner_copy['yaw'][i],
+                           planner_copy['v'][i], planner_copy['curv'][i]])
         # print(np.array(best_wp))
         # Update waypoints
         controller.update_waypoints(np.array(best_wp))
 
     ### Calculate the control signal
     long, lat, lyap_gain = controller.calculate_control_signal(delta_t, state['x'],
+    # long, lat, = controller.calculate_control_signal(delta_t, state['x'],
                                                     state['y'], state['v'],
                                                     state['yaw'])
 
     # ### Get the error profile
-    # err = controller.get_error()
+    err = controller.get_error()
     # ### Get the reference
-    # ref = controller.get_instantaneous_setpoint()
+    ref = controller.get_instantaneous_setpoint()
 
     ### Send the message
     # Header
@@ -164,7 +167,9 @@ while not rospy.is_shutdown():
     print(lat)
     # Control action
     msg.action_steer = max(min(-1*np.rad2deg(lat), max_steer_arduino), min_steer_arduino) # lat ~ (rad)
+    # msg.action_steer = 20.
     msg.action_throttle = max(min(long, max_throttle), min_throttle)
+    # msg.action_throttle = 0.1
     #msg.action_brake = max(min(-long, max_brake), 0.)
     msg.action_brake = 0.
     print("steer")